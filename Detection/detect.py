c231922b206bd37130e5bdb679fdbb7be1803b46
HiricaAlexandru <72924378+HiricaAlexandru@users.noreply.github.com>
2023-01-27 21:49:25 +0200

    AlHi/Feature disabling of detection and zipping of files

--- Detection/detect.py
+++ Detection/detect.py
@@ -4,6 +4,8 @@ import numpy as np
 from sort.sort import *
 import sys
 import os
+import zipfile
+import shutil
 
 def get_rectangles_from_pandas(results_pandas):
     rectangles = []
@@ -18,7 +20,7 @@ def get_rectangles_from_pandas(results_pandas):
 def crop_rectangles_images(image, rectangles, img_name, used_indexes):
     global output_folder, name_of_video
     video_name_without_extension = get_video_name(name_of_video)
-    output_folder_name = f"Output\\{output_folder}\\{video_name_without_extension}"
+    output_folder_name = f"Output"
     name = 0
     for rectangle in rectangles:
         #check if the id was already used
@@ -51,57 +53,65 @@ def get_video_name(video_name):
     video_name = path_video[len(path_video) - 1].split('.')[0]
     return video_name
 
-def create_folder_for_video_output(user_folder_name, video_name):
-    video_name = get_video_name(video_name)
-    os.mkdir(f"{user_folder_name}\\{video_name}")
-
-def create_user_folder_if_not_existing(output_folder_name, video_name):
-    folder_path = f"Output\\{output_folder_name}"
-    all_file = None
-    file = None
-    if os.path.exists(folder_path) == False:
-        os.mkdir(folder_path)
-        file = open(f"{folder_path}\\all_analyzed.txt", 'w+')
-    else:
-        if os.path.exists(f"{folder_path}\\all_analyzed.txt") == True:
-            file = open(f"{folder_path}\\all_analyzed.txt", 'r+')
-            all_file = file.read()
-        else:
-            file = open(f"{folder_path}\\all_analyzed.txt", 'w+')
-    
-    file.close()
-
-    if all_file != None and video_name in all_file:
-        return None
-
-    file = open(f"{folder_path}\\all_analyzed.txt", 'a')
-    file.write("\n" + video_name)
-    file.close()
-    create_folder_for_video_output(folder_path, video_name) 
-    return True
+#def create_folder_for_video_output(user_folder_name, video_name):
+#    video_name = get_video_name(video_name)
+#    os.mkdir(f"{user_folder_name}\\{video_name}")
+#
+#def create_user_folder_if_not_existing(output_folder_name, video_name):
+#    folder_path = f"Output\\{output_folder_name}"
+#    all_file = None
+#    file = None
+#    if os.path.exists(folder_path) == False:
+#        os.mkdir(folder_path)
+#        file = open(f"{folder_path}\\all_analyzed.txt", 'w+')
+#    else:
+#        if os.path.exists(f"{folder_path}\\all_analyzed.txt") == True:
+#            file = open(f"{folder_path}\\all_analyzed.txt", 'r+')
+#            all_file = file.read()
+#        else:
+#            file = open(f"{folder_path}\\all_analyzed.txt", 'w+')
+#    
+#    file.close()
+#
+#    if all_file != None and video_name in all_file:
+#        return None
+#
+#    file = open(f"{folder_path}\\all_analyzed.txt", 'a')
+#    file.write("\n" + video_name)
+#    file.close()
+#    create_folder_for_video_output(folder_path, video_name) 
+#    return True
 
 def get_system_arguments():
     name_of_video = sys.argv[1]
-    output_folder = sys.argv[2]
-    conf = float(sys.argv[3])
+    return name_of_video
 
-    return name_of_video, output_folder, conf
+def check_or_create_folder_output():
+    if os.path.isdir("Output"):
+        shutil.rmtree("Output")
+
+    os.mkdir("Output")
+
+def make_zip_file():
+    with zipfile.ZipFile("Output.zip","w") as myzip:
+        for image_name in os.listdir("Output"):
+            myzip.write(f"Output\\{image_name}")
 
 # Model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5m')
 
 # Configuring the model
-#name_of_video, output_folder, conf = get_system_arguments()
-#model.conf = conf
+name_of_video = get_system_arguments()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu") #passing the model to cpu or gpu
 
+check_or_create_folder_output()
+
 model.conf = 0.75
-#model.classes = [0]
+model.classes = [0]
 name_of_video = "Detection\\a.mp4"
-output_folder = "user"
+output_folder = ""
 
-result = create_user_folder_if_not_existing(output_folder, name_of_video)
+extract_frames_and_write_from_video(name_of_video, model)
 
-if result != None:
-    extract_frames_and_write_from_video(name_of_video, model)
+make_zip_file()
