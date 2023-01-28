203cccfcac2fcd3a3a3377c142a83b348b0dedab
IlincaGrosu <grosu.ilinca@gmail.com>
2023-01-28 23:01:44 +0200

    IlGr/BackendFinal

--- bustedAI/Controllers/VideoImportController.cs
+++ bustedAI/Controllers/VideoImportController.cs
@@ -2,11 +2,17 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.StaticFiles;
 using rentalAppAPI.DAL;
+
 using rentalAppAPI.DAL.Entities;
 using System.Diagnostics;
 using System.Net.Mime;
 using System.Text.RegularExpressions;
 using System.Web;
+using System.Text;
+using IronPython.Hosting;
+using CliWrap;
+using CliWrap.Buffered;
+using Mono.Unix.Native;
 
 namespace rentalAppAPI.Controllers
 {
@@ -35,27 +41,17 @@ namespace rentalAppAPI.Controllers
             db.VideoImports.Add(x);
             await db.SaveChangesAsync();
 
-            var filesFromPythonScyrpt = new List<string>();
-            var scriptPath = "C:/Users/grosu/PycharmProjects/Teste/test.py";
+
+            var scriptPath = "C:/Users/grosu/Documents/GitHub/proiect-inginerie-software-bustedai/Detection/detect.py";
             var arguments = filePath;
 
-            using (Process process = new Process())
-            {
-                process.StartInfo.FileName = "C:/Users/grosu/AppData/Local/Programs/Python/Python310/python.exe";
-                process.StartInfo.Arguments = string.Format("{0} {1}", scriptPath, arguments);
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.RedirectStandardOutput = true;
-                process.Start();
+            var command = await Cli.Wrap("powershell")
+                .WithWorkingDirectory(@"C:/Users/grosu/Documents/GitHub/proiect-inginerie-software-bustedai")
+                .WithArguments(new[] { "C:/Users/grosu/AppData/Local/Programs/Python/Python310/python.exe",scriptPath, filePath })
+                .ExecuteBufferedAsync();
 
-                using (StreamReader reader = process.StandardOutput)
-                {
-                    string result = reader.ReadToEnd();
-                    filesFromPythonScyrpt = result.Split(Environment.NewLine).ToList();
-                }
-            }
-            string filePath1 = Path.Combine(Directory.GetCurrentDirectory(), "App_Data", "Uploads", "demofile2.txt");
+            return PhysicalFile("C:/Users/grosu/Documents/GitHub/proiect-inginerie-software-bustedai/Output.zip", "application/octet-stream", "Output.zip");
 
-            return PhysicalFile(filePath, "application/download", "demofile2.txt");
         }
 
     }
